c212d4d30025b8db4485fe873306a1abe5e9eb21
tumscorpion <prateep2007@gmail.com>
2017-01-26 21:04:55 +0700

    change url

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,14 +37,19 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+    			'class' => 'yii\web\UrlManager',
+    			// Disable index.php
+    			'showScriptName' => false,
+    			// Disable r= routes
+    			'enablePrettyUrl' => true,
+    			'rules' => array(
+    					'<controller:\w+>/<id:\d+>' => '<controller>/view',
+    					'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+    					'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+    					'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+    			),
+    	],
         'view' => [
             'theme' => [
                 'pathMap' => [
