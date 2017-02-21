328e75f21897df13ef8dd703dc6a65fa0ca7d1d7
Msi <Msi@DESKTOP-90LO63V>
2017-02-21 13:52:54 +0700

    change color status delete

--- backend/views/project/extend.php
+++ backend/views/project/extend.php
@@ -2,6 +2,6 @@
 	$lebel[1] = "<span class='label label-success' style='padding: 2px 10px 2px;'>เปิด</span>";
 	$lebel[2] = "<span class='label label-warning' style='padding: 2px 10px 2px;'>ปิด</span>";
 	$lebel[3] = "<span class='label label-danger' style='padding: 2px 10px 2px;'>ยกเลิก</span>";
-	$lebel[4] = "<span class='label label-danger' style='padding: 2px 10px 2px;'>ถูกลบ</span>";
+	$lebel[4] = "<span class='label label-default' style='padding: 2px 10px 2px;'>ถูกลบ</span>";
 	
 ?>
